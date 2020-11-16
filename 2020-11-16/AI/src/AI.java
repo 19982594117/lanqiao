07dda6898241cb06b5820439e16e446aa154dd24
19982594117 <846083037@qq.com>
2020-11-16 21:13:37 +0800

    去除重复的repaint、hero的四个方向重新命名、让hero可以移动

---
+++ 2020-11-16/AI/src/AI.java
@@ -0,0 +1,68 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+import java.io.*;
+
+public class AI extends MIDlet
+{
+	Display display;
+	MainCanvas mc;
+	public AI(){
+		display=Display.getDisplay(this);
+		mc=new MainCanvas();
+		display.setCurrent(mc);
+	}
+	public void startApp(){
+	}
+	public void destroyApp(boolean unc){
+	}
+	public void pauseApp(){
+	}
+}
+class MainCanvas extends Canvas
+{
+	int x=100,y=120;
+	Image downImg,leftImg,upImg,rightImg,currentImg;
+	public MainCanvas(){
+		try
+		{
+			downImg=Image.createImage("/sayo10.png");
+			leftImg=Image.createImage("/sayo12.png");
+			upImg=Image.createImage("/sayo14.png");
+			rightImg=Image.createImage("/sayo16.png");
+			currentImg=downImg;
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		}
+	public void paint(Graphics g){
+		g.setColor(0,0,0);
+		g.fillRect(0,0,getWidth(),getHeight());
+		g.drawImage(currentImg,x,y,0);
+	}
+	public void keyPressed(int keyCode){
+		int action=getGameAction(keyCode);
+		if(action==LEFT){
+			currentImg=leftImg;
+			System.out.println("向左转");
+			x=x-5;
+			}
+		if(action==RIGHT){
+			currentImg=rightImg;
+			System.out.println("向右转");
+			x=x+5;
+		}
+		if(action==UP){
+			currentImg=upImg;
+			System.out.println("向上转");
+			y=y-5;
+		}
+		if(action==DOWN){
+			currentImg=downImg;
+			System.out.println("向下转");
+			y=y+5;
+		}
+		repaint();
+    }
+}
